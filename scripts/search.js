beab6e7c7025f8626c09b929a32d7465744cb4fd
Ayush <auxar@Ayushs-MacBook-Pro.local>
2016-06-21 17:01:18 +0530

    Updates in css and script

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,39 @@
 /*
-it is the jquery scripts file for search page
+This is jquery script for the page
 */
+
+var base_url = "https://api.practo.com/";
+var X_CLIENT_ID = "cb361f8c-cbf3-4ea9-ac2f-76c619a59a82";
+var X_API_KEY = "LXrzokVA14koHb+WKv/4BdRDIpU=";
+
+// this method loads the cities when document is ready to be loaded
+$(document).ready(function(){
+
+var url = base_url + "meta/search/cities";
+var citi_data = null;
+var user_country;
+
+// to know the country of user
+$.getJSON("http://freegeoip.net/json/", function(data) {
+
+});
+
+// to get all the cities supported by practo
+$.ajax({
+      type: 'GET',
+      url: url,
+      dataType: "json",
+      headers: {
+          "X-CLIENT-ID":X_CLIENT_ID,
+          "X-API-KEY":X_API_KEY
+      }
+    })
+    .success(function(data){
+
+    })
+    .error(function(error_obj,status){
+        console.log("Error calling API. Got status: " + status);
+    });
+
+
+});
