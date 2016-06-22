475ccb57f43e4e17b50d22b6dbc87998d7282d27
Ayush <auxar@Ayushs-MacBook-Pro.local>
2016-06-22 14:25:37 +0530

    added feature location fill and autocomplete

--- scripts/search.js
+++ scripts/search.js
@@ -6,34 +6,122 @@ var base_url = "https://api.practo.com/";
 var X_CLIENT_ID = "cb361f8c-cbf3-4ea9-ac2f-76c619a59a82";
 var X_API_KEY = "LXrzokVA14koHb+WKv/4BdRDIpU=";
 
-// this method loads the cities when document is ready to be loaded
+// to load content at page load
 $(document).ready(function(){
 
-var url = base_url + "meta/search/cities";
-var citi_data = null;
-var user_country;
+  var url = base_url + "meta/search/cities";
+  var user_country;
+  var country_list = [];
+  var citi_list = [];
 
-// to know the country of user
-$.getJSON("http://freegeoip.net/json/", function(data) {
+// call to get country of user
+  var geocodeCall = $.ajax({
+                        type: 'GET',
+                        dataType: 'jsonp',
+                        url: "http://freegeoip.net/json/"
+                      });
 
+// call to get all the cities supported
+  var citiCall = $.ajax({
+                      type: 'GET',
+                      url: url,
+                      dataType: "json",
+                      headers: {
+                        "X-CLIENT-ID":X_CLIENT_ID,
+                        "X-API-KEY":X_API_KEY
+                      }
+                   });
+
+
+  $.when(geocodeCall,citiCall).then(function(responseCall1,responseCall2){
+    onSuccess(responseCall1,responseCall2);
+  },onFailure);
+
+// to select only those cities which are in user city.
+  function onSuccess(responseCall1,responseCall2){
+
+      user_country = responseCall1[0].country_name;
+    //  country_list = [];
+  //    console.log(user_country);
+
+      $.each(responseCall2[0].cities,function(index,value){
+
+          var citiDetail = value;
+          country_list.push(citiDetail.state.country.name);
+
+          if(citiDetail.state.country.name==user_country){
+              $('[name=city_select]').append($('<option>',{
+                  value: citiDetail.id,
+                  text: citiDetail.name
+              }));
+          }
+      });
+
+      $.uniqueSort(country_list);
+
+      $.each(country_list,function(index){
+          if(country_list[index]==user_country){
+            $('[name=country_select]').append($('<option>',{
+                value: country_list[index],
+                text: country_list[index],
+                selected: true
+            }));
+          }
+          else{
+              $('[name=country_select]').append($('<option>',{
+                  value: country_list[index],
+                  text: country_list[index]
+              }));
+          }
+      });
+  }
+
+  var onFailure = function(){
+    console.log("call failed");
+  }
 });
 
-// to get all the cities supported by practo
-$.ajax({
-      type: 'GET',
-      url: url,
-      dataType: "json",
-      headers: {
-          "X-CLIENT-ID":X_CLIENT_ID,
-          "X-API-KEY":X_API_KEY
-      }
-    })
-    .success(function(data){
+// it fills the localities options and also enable autocomplete feature
+function fillNearbyArea(){
 
-    })
-    .error(function(error_obj,status){
-        console.log("Error calling API. Got status: " + status);
+  var city_id = $('[name=city_select]').val();
+  var url = base_url + "meta/search/cities/" + city_id;
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: "json",
+    headers: {
+      "X-CLIENT-ID":X_CLIENT_ID,
+      "X-API-KEY":X_API_KEY
+    }
+  })
+  .success(function(data){
+
+    $('[name=nearby_select]')
+            .find('option')
+            .remove()
+            .end()
+            .append('<option label="default" value="" disabled selected>Select Nearby Area</option>')
+
+    $.each(data.localities,function(index,value){
+      $('[name=nearby_select]').append($('<option>',{
+          value: value,
+          text: value
+      }));
     });
 
+    $('#searchbar').autocomplete({
+      source: function(request, response) {
+        var results = $.ui.autocomplete.filter(data.specialties, request.term);
+        response(results.slice(0, 10));
+      }
+    });
+  });
+}
 
-});
+function reloadPage(){
+
+  
+
+
+}
